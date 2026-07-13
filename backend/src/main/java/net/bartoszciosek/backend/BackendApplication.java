5c25822c66212e5df465b9f1c2e51026e19a9a1d
bartoszciosek <ciosek000@gmail.com>
2026-07-13 13:37:56 +0200

    initial

---
+++ backend/src/main/java/net/bartoszciosek/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package net.bartoszciosek.backend;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    static void main(String[] args) {
+        run(BackendApplication.class, args);
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    java
+}
+
+group = "net.bartoszciosek"
+version = "0.0.1-SNAPSHOT"
+description = "itemify"
